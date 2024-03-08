595785737848ad06643cf9ade907e8759f292e15
jskang1988 <jskang1988@gmail.com>
2024-03-08 11:50:25 +0900

    웹뷰 페이지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:melchi_and_pet/sample/firebase_sample_widget.dart';
 
 import 'app.dart';
 import 'common/data/preference/app_preferences.dart';
@@ -10,5 +9,10 @@ void main() async {
   await EasyLocalization.ensureInitialized();
   await AppPreferences.init();
 
-  runApp(FirebaseSampleWidget());
+  runApp(EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ko')],
+      fallbackLocale: const Locale('en'),
+      path: 'assets/translations',
+      useOnlyLangCode: true,
+      child: const App()));
 }
